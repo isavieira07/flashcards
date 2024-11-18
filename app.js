91604d017b1c5ed3fac8e7282ce395d27e7f4d16
isavieira07 <164920424+isavieira07@users.noreply.github.com>
2024-11-18 13:52:23 -0300

    isa

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ function criacartao(categoria, pergunta, resposta) {
 let container = document.getElementById('container')
 let cartao = document.createrElement('article')
 cartao.className = 'cartao'
+cartao.innerHTML = `
+<div class="cartao__conteudo">
+            <h3>Programação</h3>
+            <div class="cartao__conteudo__pergunta">
+                <p>O que é CSS?</p>
+            </div>
+            <div class="cartao__conteudo__resposta">
+                <p>O CSS é uma linguagem de estilização.</p>
+            </div>
+        </div>
+`
+
+container.appendChild(cartao)
 }
