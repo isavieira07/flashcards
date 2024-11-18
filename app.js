2badb5a4db044fd8ee26f2cbdbd65a2278bd9695
isavieira07 <164920424+isavieira07@users.noreply.github.com>
2024-11-18 14:24:44 -0300

    isa

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function viraCartao (){
     respostaEstaVisivel = !respostaEstaVisivel
     cartao.classList.toggle('active, respostaEstaVisivel')
 }
-cartao.addEventListenner('click', viraCartao)
+cartao.addEventiListenner('click', viraCartao)
 
 container.appendChild(cartao)
 }
